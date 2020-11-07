35cd9469a4729a9d28a496e79849c92945fb1f1a
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-11-06 22:22:27 -0500

    drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,8 @@ var createTaskEl = function(taskDataObj) {
   // add  task id as a custom attribute
   listItemEl.setAttribute("data-task-id", taskIdCounter);
 
+  listItemEl.setAttribute("draggable", "true");
+
   // creat div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
 
@@ -196,9 +198,48 @@ var taskStatusChangeHandler = function(event) {
   }
 };
 
+var dragTaskHandler = function(event) {
+  var taskId = event.target.getAttribute("data-task-id");
+  event.dataTransfer.setData("text/plain", taskId);
+  var getId = event.dataTransfer.getData("text/plain");
+  console.log("getId:", getId, typeof getId);
+};
+
+var dropZoneDragHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    event.preventDefault();
+  }
+}
+
+var dropTaskHandler = function(event) {
+  var id = event.dataTransfer.getData("text/plain");
+  var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+  var dropZoneEl = event.target.closest(".task-list");
+  var statusType = dropZoneEl.id;
+  // set status of task based on dropZone id 
+  var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+  if (statusType === "tasks-to-do") {
+    statusSelectEl.selectedIndex = 0;
+  }
+  else if (statusType === "tasks-in-progress") {
+    statusSelectEl.selectedIndex = 1;
+  }
+  else if (statusType === "tasks-completed") {
+    statusSelectEl.selectedIndex = 2;
+  }
+  dropZoneEl.appendChild(draggableElement);
+}
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+pageContentEl.addEventListener("drop", dropTaskHandler);
 
 
 
