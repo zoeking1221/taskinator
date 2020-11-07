bf3e0288127c2ee103b9f8efe2ed14d3e1f671d4
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-11-06 22:30:55 -0500

    added color background to drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -209,6 +209,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 }
 
@@ -227,8 +228,16 @@ var dropTaskHandler = function(event) {
   }
   else if (statusType === "tasks-completed") {
     statusSelectEl.selectedIndex = 2;
-  }
+  };
+  dropZoneEl.removeAttribute("style");
   dropZoneEl.appendChild(draggableElement);
+}
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -240,6 +249,8 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
 
 
